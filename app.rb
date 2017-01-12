c32c655cf00d1a6222bce6b3780d5221242b17e2
Chris Severns <severnsc@gmail.com>
2017-01-12 16:24:08 -0500

    Add crack_cipher

--- app.rb
+++ app.rb
@@ -15,15 +15,48 @@ class CaesarCipher
 					letter = alphabet[index-shift]
 				end
         	else
-          		letters
+          		letter
 			end
 		end
         word = letters.join
 		word
 	end
 
+	def self.crack_caesar_cipher cipher
+		alphabet = ("a".."z").to_a
+		i = 1
+		possibles = []
+		while i<26
+			letters = cipher.downcase.split("")
+			letters.collect! do |letter|
+				if /[A-Za-z]/.match(letter) != nil
+					index = alphabet.index(letter)
+					letter = alphabet[index-i]
+				else
+				letter
+				end
+			end
+			possibles << letters.join
+			i+=1
+		end
+		possibles
+	end
+
 end
 
 get "/" do
-	erb :index
+	if params[:string] && params[:shift]
+		string = params[:string]
+		shift = params[:shift].to_i
+		cipher = CaesarCipher.caesar_cipher(string, shift)
+		cleartext = ""
+	elsif params[:cipher]
+		cipher_text = params[:cipher]
+		cleartext = CaesarCipher.crack_caesar_cipher(cipher_text)
+		cipher = ""
+	else
+		cipher = ""
+		cleartext = ""
+	end
+	erb :index, :locals => {:cipher => cipher, :cleartext => cleartext}
 end
