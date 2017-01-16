71a809a7f47e682a75458dd1be33a2d5f62d89da
Chris Severns <severnsc@gmail.com>
2017-01-16 15:06:48 -0500

    Add history of guesses and the feedback from those guesses when human is guesser

--- app.rb
+++ app.rb
@@ -38,7 +38,12 @@ post "/mastermind/game" do
 	setter = session[:setter]
 	session[:guesses] = 12
 	session[:feedback] = ""
-	setter == "guess" ? session[:colors] = AI.set_master_code : session[:board_set] = "false"
+	if setter == "guess" 
+		session[:colors] = AI.set_master_code 
+		session[:guess_feedback_history] = []
+	else 
+		session[:board_set] = "false"
+	end
 	redirect("/mastermind/game")
 end
 
@@ -50,7 +55,7 @@ get "/mastermind/game" do
 	@board_set = session[:board_set]
 	@guesses = session[:guesses].to_i
 	if setter == "guess"
-		@last_guess = session[:last_guess]
+		@guess_feedback_history = session[:guess_feedback_history]
 	elsif setter == "set" && @board_set == "true"
 		AI.remember_feedback(@feedback) unless @feedback.empty?
 		@guess = AI.guess(@guesses)
@@ -74,9 +79,9 @@ post "/mastermind/feedback" do
 		feedback = [params[:color1_feedback], params[:color2_feedback], params[:color3_feedback], params[:color4_feedback]]
 	else
 		guess = [params[:color1_guess], params[:color2_guess], params[:color3_guess], params[:color4_guess]]
-		session[:last_guess] = guess
 		session[:guesses] = guesses - 1
 		feedback = AI.feedback(guess)
+		session[:guess_feedback_history] << [guess, feedback]
 	end
 	if setter == "set" && feedback == ["X", "X", "X", "X"]
 		redirect("/mastermind/lose")
@@ -94,10 +99,10 @@ end
 
 get "/mastermind/win" do
 	name = session[:name]
-	erb :mastermind_win, locals => {:name => name}
+	erb :mastermind_win, :locals => {:name => name}
 end
 
 get "/mastermind/lose" do
 	name = session[:name]
-	erb :mastermind_lose, locals => {:name => name}
+	erb :mastermind_lose, :locals => {:name => name}
 end
