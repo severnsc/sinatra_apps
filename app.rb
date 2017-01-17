097147454be6da296271e30dbcd7269f7474af75
Chris Severns <severnsc@gmail.com>
2017-01-17 12:09:04 -0500

    Add styling to Mastermind play and win/lost pages. Fix bugs in game logic

--- app.rb
+++ app.rb
@@ -38,6 +38,7 @@ post "/mastermind/game" do
 	setter = session[:setter]
 	session[:guesses] = 12
 	session[:feedback] = ""
+	session[:last_guess] = ""
 	if setter == "guess" 
 		session[:colors] = AI.set_master_code 
 		session[:guess_feedback_history] = []
@@ -54,11 +55,14 @@ get "/mastermind/game" do
 	@colors = session[:colors]
 	@board_set = session[:board_set]
 	@guesses = session[:guesses].to_i
+	last_guess = session[:last_guess]
 	if setter == "guess"
 		@guess_feedback_history = session[:guess_feedback_history]
 	elsif setter == "set" && @board_set == "true"
 		AI.remember_feedback(@feedback) unless @feedback.empty?
-		@guess = AI.guess(@guesses)
+		print AI.colors
+		@guess = AI.guess(@guesses, @feedback, last_guess)
+		session[:last_guess] = @guess
 		session[:guesses] = @guesses - 1
 	end
 	erb :mastermind_game, :locals => {:name => name, :setter => setter}
@@ -75,12 +79,13 @@ end
 post "/mastermind/feedback" do
 	setter = session[:setter]
 	guesses = session[:guesses].to_i
+	master = session[:colors]
 	if setter == "set"
 		feedback = [params[:color1_feedback], params[:color2_feedback], params[:color3_feedback], params[:color4_feedback]]
 	else
 		guess = [params[:color1_guess], params[:color2_guess], params[:color3_guess], params[:color4_guess]]
 		session[:guesses] = guesses - 1
-		feedback = AI.feedback(guess)
+		feedback = AI.feedback(guess, master)
 		session[:guess_feedback_history] << [guess, feedback]
 	end
 	if setter == "set" && feedback == ["X", "X", "X", "X"]
--- model/AI.rb
+++ model/AI.rb
@@ -1,63 +1,66 @@
 class AI
+	class << self; attr_reader :colors end
 
-	@@colors = ["red", "blue", "green", "yellow", "white", "orange"]
+	@colors = ["red", "blue", "green", "yellow", "white", "orange"]
 	@@feedback = ""
-	@@guess = []
-	@@master = []
-	@@master_hash = Hash.new(0)
 
 	def self.set_master_code
-		@@master = []
-		4.times {@@master.push(@@colors[rand(6)])}
-		@@master.each {|color| @@master_hash[color] += 1}
-		@@master
+		colors = ["red", "blue", "green", "yellow", "white", "orange"]
+		master = []
+		master_hash = Hash.new(0)
+		4.times {master.push(colors[rand(6)])}
+		master.each {|color| master_hash[color] += 1}
+		master
 	end
 
-	def self.feedback(guess)
+	def self.feedback(guess, master)
 		correct = Hash.new(0)
+		master_hash = Hash.new(0)
+		master.each {|color| master_hash[color] += 1}
 		feedback = ["", "", "", ""]
 		guess.each_with_index do |g ,index|
-			if g == @@master[index]
+			if g == master[index]
 				feedback[index] = "X"
 				correct[g] += 1
 			end
 		end
 		guess.each_with_index do |g, index|
 			if feedback[index] != "X"
-				correct[g] < @@master_hash[g] ? feedback[index] = "O" : feedback[index] = "W"
+				correct[g] < master_hash[g] ? feedback[index] = "O" : feedback[index] = "W"
 			end
 		end
 		feedback
 	end
 
-	def self.guess(guesses)
+	def self.guess(guesses, feedback, guess)
 		if guesses == 12
-			@@guess = ["red", "blue", "green", "yellow"]
+			@colors = ["red", "blue", "green", "yellow", "white", "orange"]
+			guess = ["red", "blue", "green", "yellow"]
 		else
-			@@feedback.each_with_index do |f, index|
+			feedback.each_with_index do |f, index|
 				if f == "W"
-					@@colors.delete(@@guess[index])
+					@colors.delete(guess[index])
 				end
 			end
-			@@feedback.each_with_index do |f, index|
+			feedback.each_with_index do |f, index|
 				if f == "W"
-					@@guess[index] = @@colors[rand(@@colors.count)]
+					guess[index] = @colors[rand(@colors.count)]
 				end
 			end
 			colors_to_swap = []
-			@@feedback.each_with_index do |f, index|
+			feedback.each_with_index do |f, index|
 				if f != "X"
-					colors_to_swap.push(@@guess[index])
+					colors_to_swap.push(guess[index])
 				end
 			end
-			@@feedback.each_with_index do |f, index|
+			feedback.each_with_index do |f, index|
 				if f != "X"
-					@@guess[index] = colors_to_swap[-1]
+					guess[index] = colors_to_swap[rand(colors_to_swap.count)]
 					colors_to_swap.pop
 				end
 			end
 		end
-		@@guess
+		guess
 	end
 
 	def self.remember_feedback(feedback)
